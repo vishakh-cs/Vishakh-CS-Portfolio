367555dc0d93ca746f34d156cbbd74c04d25feaf
vishakh-cs <143394085+vishakh-cs@users.noreply.github.com>
2024-05-18 10:50:27 +0530

    update

--- Components/Dialog.jsx
+++ Components/Dialog.jsx
@@ -7,7 +7,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog";
+} from "@/Components/ui/dialog"
 
 export function ContactDialog() {
   const emailAddress = "Vishakhcs51@gmail.com";
