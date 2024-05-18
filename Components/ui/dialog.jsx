154dafa9e6be254c9d6aef61cc56f05cd4504be5
vishakh-cs <143394085+vishakh-cs@users.noreply.github.com>
2024-05-18 09:50:28 +0530

    projects-added

--- Components/ui/dialog.jsx
+++ Components/ui/dialog.jsx
@@ -7,7 +7,6 @@ import { X } from "lucide-react"
 import { cn } from "@/lib/utils"
 
 const Dialog = DialogPrimitive.Root
-
 const DialogTrigger = DialogPrimitive.Trigger
 
 const DialogPortal = DialogPrimitive.Portal
