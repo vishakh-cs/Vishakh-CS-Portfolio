e1e30a4e13fc301b8b5328096f49c66deef2ccb7
vishakh-cs <143394085+vishakh-cs@users.noreply.github.com>
2024-05-21 17:07:29 +0530

    purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
   darkMode: ["class"],
   content: [
     './pages/**/*.{js,jsx}',
